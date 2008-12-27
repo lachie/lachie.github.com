3923dd6b441e3ffd6e54736cfc0e967bb790d192
lachie <lachiec@gmail.com>
2008-12-27 21:50:27 +1100

    shows top repos

--- gh.js
+++ gh.js
@@ -1,15 +1,18 @@
 $(function() {
-  var limit = 4           // how many repos to list
+  var limit = 4        // how many repos to list
   var login = 'lachie' // your username
   $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
     var repos = $.grep(data.user.repositories, function() {
       return !this.fork
     })
-    repos.sort(function(a, b) {
-      return b.watchers - a.watchers
+    .sort(function(a, b) {
+      return a.watchers - b.watchers
     })
-    $.each(repos.reverse().slice(0, limit), function() {
+    .reverse()
+    .slice(0, limit);
+    
+    $.each(repos, function() {
       $('#repos').append('<li class="repo"><a href="' + this.url + '">&nbsp;- ' + this.name + '</a></li>')
-    })
+    });
   })        
 })
